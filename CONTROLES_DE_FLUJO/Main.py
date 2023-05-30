77702225a89f2f0242349ad598a4a96cd1ab2359
cristians02 <cristiandelmar06@gmail.com>
2023-05-30 09:42:24 -0500

    ultimet

--- CONTROLES_DE_FLUJO/Main.py
+++ CONTROLES_DE_FLUJO/Main.py
@@ -10,15 +10,28 @@
 #evaluar si es menor de 17 monstrar com mensaje cana si es mayor a 18 monstara come y si es mayor a 40 monstrar ya esta usado
 
 #entrada de datos
-edad= input ('ingrese un edad: ')
-if edad < 18 :
-    print('cana')
+dead= input ('ingrese un edad: ')
+if edad < 17 :
+  print('cana')
 if edad > 40 :
-    print('ya esta usado')
+  print('ya esta usado')
     
     
 # hacer  un programa que pida al usuario su dni si la longuitud del dni es 8  que pida Su nombre  y lo muestre por consola si la longuitud del dni es mayor o menor a 8 que le  mjestre un mensaje de erorr
+#datos de entrada
+dniUsuario= input(" Ingrese si DNI: ")
+longitudDni=len(dniUsuario)
+#proceso
+if longitudDni== 8 :
+    #entrada
+    nombre=input("Ingresa tu nombre: ")
+    #proceso
+    mensaje=f"""hola bienvenido , {nombre}"""
+    #salida de dato
+    print(mensaje)
+else:
+    print("El dni ingresado es incorrecto ")
 
-DNI=input ('ingrese su numero de DNI: ')
-if DNI (longitud ) :
-    else
+
+
+# hacer un programa que pida al usuario ingresar el primer apellido si su apellido tiene en como ultimos caractereslas letras--ez-- mostrar un mensaje que diga casi eres español si los carcteres finales  son --es--  que diga eres casi peruano
