6dd9108c7ebac7e6ab0222742ad392ad2f1aaeb0
cristians02 <cristiandelmar06@gmail.com>
2023-06-07 23:14:56 -0500

    tareita  de alvarez

---
+++ CONTROLES_DE_FLUJO/juegoxd.py
@@ -0,0 +1,18 @@
+while True:
+    jugador1 = int (input("jugador 1 : 1=piedra, 2= papel, 3=tigera: ") )
+    jugador2 = int (input("jugador 2 : 1=piedra, 2= papel, 3=tigera: ") )
+    
+    if jugador1== 1 and jugador2 == 3:
+        print('jugador 1 Gana: piedra gana a tigera')
+    elif jugador1 == 2 and jugador2 == 1:
+        print('jugador 2 gana: papel gana a piedra')
+    elif jugador1 == 2 and jugador2 == 2:
+        print('jugador 3 gana: tijera gana a papel')
+    elif jugador1== 1 and jugador2 == 3:
+        print('jugador 1 Gana: piedra gana a tigera')
+    elif jugador1 == 2 and jugador2 == 1:
+        print('jugador 2 gana: papel gana a piedra')
+    elif jugador1 == 2 and jugador2 == 2:
+        print('jugador 3 gana: tijera gana a papel')
+    else:
+        print("ninguno gana")
