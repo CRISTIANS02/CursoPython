ef7388179c1d837a015b6efe3f4532b32eb87abc
cristians02 <cristiandelmar06@gmail.com>
2023-06-23 10:05:10 -0500

    ejercicios de lista -23/06/2023

---
+++ CONTROLES_DE_FLUJO/ejercicio lista 2.py
@@ -0,0 +1,11 @@
+lista = []
+while True:
+    dato = input("Ingresa un dato: ")
+  
+    if dato.lower() == "si":
+        
+# Elimina la palabra "si" de la lista
+        break
+    lista.append(dato)
+print("La lista completa es:")
+print(lista)
